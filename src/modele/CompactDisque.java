cc0f39f53299267ad018e1d172ea5afaa5936622
Meltirys <mat.bourbon@hotmail.fr>
2026-09-24 15:43:37 +0200

    Adding childs of Album

---
+++ src/modele/CompactDisque.java
@@ -0,0 +1,36 @@
+package modele;
+
+import java.time.LocalDate;
+
+public class CompactDisque extends Album{
+
+    String numero;
+    String type;
+
+    public CompactDisque(String nom, String artiste, LocalDate date, int qte, String numero, String type) {
+        super(nom, artiste, date, qte);
+        this.numero = numero;
+        this.type = type;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getSupport() {
+        return "";
+    }
+}
---
+++ src/modele/DisqueVinyle.java
@@ -0,0 +1,36 @@
+package modele;
+
+import java.time.LocalDate;
+
+public class DisqueVinyle extends Album{
+
+    String numero;
+    Integer taille;
+
+    public DisqueVinyle(String nom, String artiste, LocalDate date, int qte, String numero, Integer taille) {
+        super(nom, artiste, date, qte);
+        this.numero = numero;
+        this.taille = taille;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Integer getTaille() {
+        return taille;
+    }
+
+    public void setTaille(Integer taille) {
+        this.taille = taille;
+    }
+
+    @Override
+    public String getSupport() {
+        return "";
+    }
+}
