3ab93853aa2e38dbf7dae05fbb66b75d3e892435
Florian <florian.dev56@gmail.com>
2026-09-24 13:45:26 +0200

    classe Album

--- src/modele/Album.java
+++ src/modele/Album.java
@@ -2,9 +2,59 @@ package modele;
 
 import java.time.LocalDate;
 
-public class Album {
+public abstract class Album {
     private String nom;
     private String artiste;
     private LocalDate Date;
     private int qte;
+
+    public Album(String nom, String artiste, LocalDate date, int qte) {
+        this.nom = nom;
+        this.artiste = artiste;
+        Date = date;
+        this.qte = qte;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getArtiste() {
+        return artiste;
+    }
+
+    public void setArtiste(String artiste) {
+        this.artiste = artiste;
+    }
+
+    public LocalDate getDate() {
+        return Date;
+    }
+
+    public void setDate(LocalDate date) {
+        Date = date;
+    }
+
+    public int getQte() {
+        return qte;
+    }
+
+    public void setQte(int qte) {
+        this.qte = qte;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "nom='" + nom + '\'' +
+                ", artiste='" + artiste + '\'' +
+                ", Date=" + Date +
+                ", qte=" + qte +
+                '}';
+    }
+
 }
